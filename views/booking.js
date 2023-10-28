05067d74bc6f697a8e2c0aca15edb7099454c00f
qtnguyen1611 <142379474+qtnguyen1611@users.noreply.github.com>
2023-10-29 03:04:39 +0800

    filter for subject done left with location

--- views/booking.js
+++ views/booking.js
@@ -1,9 +1,21 @@
-//Retrieve from databa the tutors with the Status = "Available"
+//Retrieve tutors with active = true"
 let tutorProfiles = [
     {tutorID: 'nnnnn', name: 'Thomas', sex: 'm', profileURL: './pictures/thomas.png', nearestMRT: 'Yishun', contactNo: '91234567', subject: ['Math', 'English', 'Science'], active: true},
-    {tutorID: 'xxxxx', name: 'Cheryl', sex: 'f', profileURL: './pictures/cheryl.png', nearestMRT: 'Bishan', contactNo: '91234568', subject: ['Science', 'English'], active: true}
+    {tutorID: 'xxxxx', name: 'Cheryl', sex: 'f', profileURL: './pictures/cheryl.png', nearestMRT: 'Bishan', contactNo: '91234568', subject: ['Science', 'English'], active: true},
+    {tutorID: 'nnnnn1', name: 'Cong', sex: 'm', profileURL: './pictures/thomas.png', nearestMRT: 'Yishun', contactNo: '91234567', subject: ['Math', 'English', 'Science'], active: true},
+    {tutorID: 'xxxxx1', name: 'Josh', sex: 'f', profileURL: './pictures/cheryl.png', nearestMRT: 'Bishan', contactNo: '91234568', subject: ['Science', 'English'], active: true}
+
 ]
 
+
+//new set of data for testing filter
+let tutorProfiles1 = [
+    {tutorID: 'nnnnkxca', name: 'Kelvin', sex: 'm', profileURL: './pictures/thomas.png', nearestMRT: 'Yishun', contactNo: '91234567', subject: ['Math', 'English', 'Science'], active: true},
+    {tutorID: 'xxxxxascasc', name: 'Jing Jia', sex: 'f', profileURL: './pictures/cheryl.png', nearestMRT: 'Bishan', contactNo: '91234568', subject: ['Science'], active: true},
+    {tutorID: 'nnnnn1asdac', name: 'Cong', sex: 'm', profileURL: './pictures/thomas.png', nearestMRT: 'Yishun', contactNo: '91234567', subject: ['Math', 'English', 'Science'], active: true},
+    {tutorID: 'xxxxx1asdasd', name: 'Josh', sex: 'f', profileURL: './pictures/cheryl.png', nearestMRT: 'Bishan', contactNo: '91234568', subject: ['Science', 'English'], active: true}
+
+]
 
 let tuitionLising = [
     {listingID: 'abc', tutorID: 'nnnnn', date: '23/11/2023', time: '3PM-5PM', status: 'available'},
@@ -13,10 +25,12 @@ let tuitionLising = [
     {listingID: 'xyznb', tutorID: 'xxxxx', date: '22/11/2023', time: '7PM-9PM', status: 'available'}
 ]
 
+//store the current user Infor
 let parentProfile = {
     parentID: '2222', name: 'Nickel', sex: 'm', nearestMRT: 'Ang Mo Kio', childEducationLevel: 'Primary', childSex: 'f', childName: 'Kim', subject: ['Math', 'English'] 
 }           
 
+//Retrieve mrtCoordinates
 let mrtCoordinates = {
     Yishun: [1.429666, 103.835044],
     Bishan: [1.35092, 103.848206],
@@ -35,10 +49,55 @@ const app = Vue.createApp({
         return {
             tutors: [], //Store Tutor Profiles to display
             selected: null, //store selected Tuition Listing
-            notes: '' //parent notes to tutor
+            notes: '', //parent notes to tutor
+
+            filter: {
+                eduLevel: 'primary'
+            },
+
+            eduLevels: {
+                primary: {
+                    key: 'primary',
+                    string: 'Primary',
+                    subjects: ['Primary Math', 'Primary English', 'Primary Science', 'Primary Chinese', 'Primary Higher Chinese']
+                },
+                lowerSec: {
+                    key: 'lowerSec',
+                    string: 'Lower Secondary',
+                    subjects: ['Lower Sec Math', 'Lower Sec English', 'Lower Sec Science', 'Lower Sec Higher Chinese', 'Lower Sec Geography', 'Lower Sec History', 'Lower Sec Literature']
+                },
+                upperSec: {
+                    key: 'upperSec',
+                    string: 'Upper Secondary',
+                    subjects: ['Upper Sec E-Math', 'Upper Sec A-Math', 'Upper Sec English', 'Upper Sec Physics', 'Upper Sec Chemistry', 'Upper Sec Biology', 'Upper Sec Chinese', 'Upper Sec Higher Chinese', 'Upper Sec Geography', 'Upper Sec History', 'Upper Sec Social Studies', 'Upper Sec Literature', 'Principles of Accounts']
+                }
+            },
+
+            filter: {
+                subjects: ['Primary Math', 'Primary English', 'Primary Science', 'Primary Chinese', 'Primary Higher Chinese'],
+                eduLevel: 'primary'
+            },
+
+            filterResult: {
+                subject: 'Primary Math',
+                region: '',
+            },
+
+            //sort MRT stations based on region for filter
+            regions: ['North']
+
+            
         }
     },    
     methods: {
+        //change subjects filter based on the education level
+        changeSubjects() {
+            let key = this.filter.eduLevel
+            let subjects = this.eduLevels[key].subjects
+            this.filterResult.subject = subjects[0]
+            this.filter.subjects = subjects
+        },
+        
         //convert array subjects to string
         handleSubjectArray() {
             for (tutor of this.tutors) {
@@ -61,7 +120,6 @@ const app = Vue.createApp({
                 }
                 //assign the value back the object
                 tutor['subject'] = subjectStr
-                console.log(subjectStr)      
             }
         },
         //add a list of tuition listing of each tutor into data tutors as a key value pair timeslot: []
@@ -122,6 +180,17 @@ const app = Vue.createApp({
             //update the status of the Tuition Listing to pending
             //Check the Tuition Listings of the Tutor (to make sure that we only render tutors who have available tuition listing)
                 //If null => set the 'active' of the tutor to 'false'
+        },
+
+        handleFilter() {
+            console.log(this.filterResult.subject, this.filterResult.region)
+            //retrieve from the database the tutors that matches the filter
+            //push the array of object into this.tutors to render
+            this.tutors = tutorProfiles1
+            this.handleTimeSlot()
+            this.handleSubjectArray()
+            this.calculateDistance()
+            console.log(this.tutors)
         },
 
         resetSelected() {
